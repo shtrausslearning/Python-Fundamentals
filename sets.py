9b736df932a847c6b3d0e72b1901e84b5a1aef3c
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-04-23 12:41:47 +0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -75,3 +75,16 @@ x1.symmetric_difference(x2)
 
 x1 ^ x2
 # {'foo', 'qux', 'quux', 'bar'}
+
+# Iterating through a Set
+
+odd_list = [1, 3, 5, 7]
+unordered_set = {9, 10, 11, 12, 13, 14, 15, 16, 17}
+
+print(unordered_set)
+
+for num in unordered_set:
+    if(not num % 2 == 0):
+        odd_list.append(num)
+
+print(odd_list)
