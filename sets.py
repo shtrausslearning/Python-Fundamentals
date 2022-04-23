1fd5ce37e64803f182c40d5299fbd6639ab5dfce
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-04-23 12:43:57 +0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -81,10 +81,11 @@ x1 ^ x2
 odd_list = [1, 3, 5, 7]
 unordered_set = {9, 10, 11, 12, 13, 14, 15, 16, 17}
 
-print(unordered_set)
-
 for num in unordered_set:
     if(not num % 2 == 0):
         odd_list.append(num)
 
+print(unordered_set)
 print(odd_list)
+# {9, 10, 11, 12, 13, 14, 15, 16, 17}
+# [1, 3, 5, 7, 9, 11, 13, 15, 17]
