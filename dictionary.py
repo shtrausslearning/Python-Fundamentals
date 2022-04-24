5fe078f1214c7c4455e5f981683b83091256aa19
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-04-24 21:04:40 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+# Remove items
+
+phone_book = {"Batman": 468426,
+              "Cersei": 237734,
+              "Ghostbusters": 44678}
+print(phone_book)
+
+cersei = phone_book.pop("Cersei")
+print(phone_book)
+print(cersei)
+
+lst = [2,4,7]
+lst.pop()
+print(lst)
+lst.remove(2)
+print(lst)
+
+# Removes and returns the last inserted pair, as a tuple
+# In Python versions before 3.7, popitem() removes and returns the random item
+lastAdded = phone_book.popitem()
+print(lastAdded)
