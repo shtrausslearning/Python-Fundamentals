b9d648b64cbde32e3515d3d80ce11158bcf5318f
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-04-24 21:08:29 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -19,3 +19,14 @@ print(lst)
 # In Python versions before 3.7, popitem() removes and returns the random item
 lastAdded = phone_book.popitem()
 print(lastAdded)
+
+# Dictionary Comprehension
+
+houses = {1: "Gryffindor", 2: "Slytherin", 3: "Hufflepuff", 4: "Ravenclaw"}
+
+for (i,j) in houses.items():
+  print(i,j)
+  
+new_houses = {n**2: house + "!" for (n, house) in houses.items()}
+print(houses)
+print(new_houses)
