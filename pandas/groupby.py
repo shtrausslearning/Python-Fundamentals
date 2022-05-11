84a41d57f1759b0fc71497b6892c02643c93036a
Andrey <79809117+shtrausslearning@users.noreply.github.com>
2022-05-11 22:10:54 +0300

    Update groupby.py

--- pandas/groupby.py
+++ pandas/groupby.py
@@ -5,6 +5,11 @@ print("---------------------------------")
 # method 2, use the native function to sum the price for each group
 print(g.sum()["price"])
 
+df = pd.read_csv("sample2.csv", sep=",")
+g = df.groupby("name")
+print("calculate the sum/mean/std for name group price column")
+print(g["price"].agg([np.sum, np.mean, np.std]))
+
 # iterate groupby
 
 df = pd.read_csv("sample2.csv", sep=",")
